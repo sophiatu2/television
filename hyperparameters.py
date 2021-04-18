8b39cfb1f39f57f5d3a0694ebe60a9af1fbb6461
Alex Le <alexle@Alexs-MacBook-Pro.local>
2021-04-17 17:09:26 -0700

    kelly

--- hyperparameters.py
+++ hyperparameters.py
@@ -7,7 +7,7 @@ Number of epochs. If you experiment with more complex networks you
 might need to increase this. Likewise if you add regularization that
 slows training.
 """
-num_epochs = 50
+num_epochs = 10
 
 """
 A critical parameter that can dramatically affect whether training
